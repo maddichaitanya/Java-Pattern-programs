48600bf37ef7436bf3a271390504317d8dcc1209
Maddi Chaitanya <150575674+maddichaitanya@users.noreply.github.com>
2026-06-04 19:56:44 +0530

    Add files via upload

---
+++ space-Pattern/SpacePattern.java
@@ -0,0 +1,35 @@
+class SpacePattern{
+	public static void main(String[] args){
+
+/* 
+	    *
+	   **
+	  ***
+	 ****
+	*****
+*/
+		int n=5;
+		for(int i=1;i<=n;i++){
+			for(int j=i;j<n;j++){
+				System.out.print("_");
+			}
+			for(int j=i;j>=1;j--){
+				System.out.print("*");
+			}
+		System.out.println();
+		}
+		System.out.println("hello");
+
+		// or 
+
+		for(int i=n;i>=1;i--){
+			for(int j=i;j>1;j--){
+				System.out.print("_");
+			}
+			for(int j=i;j<=n;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
