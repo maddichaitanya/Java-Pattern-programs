6d3a8a2d7c2ffe61efab20c47fa7b01670b19655
Maddi Chaitanya <150575674+maddichaitanya@users.noreply.github.com>
2026-06-04 20:23:04 +0530

    Update SpacePattern.java

--- space-Pattern/SpacePattern.java
+++ space-Pattern/SpacePattern.java
@@ -11,20 +11,65 @@ class SpacePattern{
 		int n=5;
 		for(int i=1;i<=n;i++){
 			for(int j=i;j<n;j++){
-				System.out.print("_");
+				System.out.print(" ");
 			}
 			for(int j=i;j>=1;j--){
 				System.out.print("*");
 			}
 		System.out.println();
 		}
-		System.out.println("hello");
+		System.out.println();
 
-		// or 
+		// or /
+
+	/*
+		*
+	   **
+	  ***
+	 ****
+	*****
+*/
 
 		for(int i=n;i>=1;i--){
 			for(int j=i;j>1;j--){
-				System.out.print("_");
+				System.out.print(" ");
+			}
+			for(int j=i;j<=n;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+/*
+	*****
+	 ****
+	  ***
+	   **
+	    *
+*/
+		System.out.println();
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<i;j++){
+				System.out.print(" ");
+			}
+			for(int j=i;j<=n;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+/*
+	*****
+	 ****
+	  ***
+	   **
+	    *
+*/
+
+		System.out.println();
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=i;j++){
+				System.out.print(" ");
 			}
 			for(int j=i;j<=n;j++){
 				System.out.print("*");
