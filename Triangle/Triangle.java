0fe1e3e4bc95b0a6020d54f0c6ec1812715531c0
Maddi Chaitanya <150575674+maddichaitanya@users.noreply.github.com>
2026-06-24 16:51:44 +0530

    Refactor Triangle class and fix nested loops

--- Triangle/Triangle.java
+++ Triangle/Triangle.java
@@ -13,14 +13,13 @@ class Triangle{
 		
 		for(int i=1;i<=n;i++){
 			
-			for(int j=1;j<=n;j++){
+			for(int j=1;j<=n+i-1;j++){
 				if(j<n-i+1)
 					System.out.print(" ");	
 				else
 					System.out.print("*");
 			}
-			for(int j=1;j<i;j++)
-				System.out.print("*");
+			
 			System.out.println();
 		}
 
